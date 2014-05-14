d7083adc7448f8d4a793cc432de94121c26ed6ec
Brecht Bonte <nbz_tricksta@hotmail.com>
2014-05-14 07:57:11 +0200

    added config file

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ var CONFIG = (function() {
         INTERVAL: 5000,
         FETCH_URL: '/fetch.php',
         CONSOLE_URL: '/console-text.php',
-        BUILD_URL: '/build.php'
+        BUILD_URL: '/build.php',
+        COLOR_ORDER: ['red', 'red_anime', 'yellow', 'yellow_anime','aborted', 'aborted_anime','blue', 'blue_anime','disabled', 'disabled_anime']
     };
 
     return {
