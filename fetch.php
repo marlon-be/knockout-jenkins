e40350076f1b1988c14d7c71ce960db2f13b081d
Dieter Provoost <contact@dieterprovoost.be>
2015-11-19 16:03:08 +0100

    use jenkins URL

--- fetch.php
+++ fetch.php
@@ -1,3 +1,3 @@
 <?php
 header('Content-Type: application/json');
-echo file_get_contents('http://192.168.0.102:8080/api/json?pretty=true&depth=1');
+echo file_get_contents('https://godfather:godfather@jenkins.marlon.be/api/json?pretty=true&depth=1');
