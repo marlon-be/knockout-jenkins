32e0cf3a7f9155886f4873fc52352bed8ea8a259
Brecht Bonte <nbz_tricksta@hotmail.com>
2015-11-25 13:22:27 +0100

    set all php pages to use new jenkins url

--- build.php
+++ build.php
@@ -2,5 +2,5 @@
 $jobName = isset($_GET['job']) ? $_GET['job'] : '';
 if ($jobName) {
     header('Content-Type: application/json');
-    echo file_get_contents('http://192.168.0.102:8080/job/' . $jobName . '/lastBuild/api/json');
+    echo file_get_contents('https://godfather:godfather@jenkins.marlon.be/job/' . $jobName . '/lastBuild/api/json');
 }
--- progress.php
+++ progress.php
@@ -2,5 +2,5 @@
 $jobName = isset($_GET['job']) ? $_GET['job'] : '';
 if ($jobName) {
     header('Content-Type: application/json');
-    echo file_get_contents('http://192.168.0.102:8080/job/' . $jobName . '/lastBuild/api/xml?depth=1&xpath=*/executor/progress/text()');
+    echo file_get_contents('https://godfather:godfather@jenkins.marlon.be/job/' . $jobName . '/lastBuild/api/xml?depth=1&xpath=*/executor/progress/text()');
 }
